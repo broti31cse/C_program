2a3b3b635912668a9431446be8a1746f03bf353e
Broti Adhikary <brotiadhikary31@gmail.com>
2026-09-10 15:03:17 +0530

    Add files via upload

---
+++ Assignment_7(10.09.26)/postfixEvaluation.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <ctype.h>
+
+#define MAX 100
+
+int stack[MAX];
+int top = -1;
+
+void push(int x)
+{
+    stack[++top] = x;
+}
+
+int pop()
+{
+    return stack[top--];
+}
+
+int evaluatePostfix(char postfix[])
+{
+    int i, a, b;
+
+    for (i = 0; postfix[i] != '\0'; i++)
+    {
+        if (isdigit(postfix[i]))
+        {
+            push(postfix[i] - '0');
+        }
+        else
+        {
+            b = pop();
+            a = pop();
+
+            switch (postfix[i])
+            {
+                case '+':
+                    push(a + b);
+                    break;
+
+                case '-':
+                    push(a - b);
+                    break;
+
+                case '*':
+                    push(a * b);
+                    break;
+
+                case '/':
+                    push(a / b);
+                    break;
+            }
+        }
+    }
+
+    return pop();
+}
+
+int main()
+{
+    char postfix[MAX];
+
+    printf("Enter postfix expression: ");
+    scanf("%s", postfix);
+
+    printf("Result = %d", evaluatePostfix(postfix));
+
+    return 0;
+}
